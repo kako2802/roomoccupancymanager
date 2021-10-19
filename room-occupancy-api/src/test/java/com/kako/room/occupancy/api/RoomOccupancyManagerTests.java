33bcfabdbe187162af8ef241a6512650ba662bad
Karol Kowalik <karol.kowalik.280285@gmail.com>
2021-10-19 20:44:52 +0200

    002-create-room-occupancy-api externalize guests prices to a file

--- room-occupancy-api/src/test/java/com/kako/room/occupancy/api/RoomOccupancyManagerTests.java
+++ room-occupancy-api/src/test/java/com/kako/room/occupancy/api/RoomOccupancyManagerTests.java
@@ -1,36 +1,46 @@
 package com.kako.room.occupancy.api;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoomOccupancyManagerTests {
 
-    private static final List<BigDecimal> GUESTS_PRICES =
-            Arrays.asList(
-                    BigDecimal.valueOf(23),
-                    BigDecimal.valueOf(45),
-                    BigDecimal.valueOf(155),
-                    BigDecimal.valueOf(374),
-                    BigDecimal.valueOf(22),
-                    BigDecimal.valueOf(99.99),
-                    BigDecimal.valueOf(100),
-                    BigDecimal.valueOf(101),
-                    BigDecimal.valueOf(115),
-                    BigDecimal.valueOf(209));
+    private static final BigDecimal ROOM_CATEGORY_DELIMITER = BigDecimal.valueOf(100);
+
+    private static List<BigDecimal> guestsPrices;
+
+    @BeforeAll
+    private static void readGuestsPrices() throws URISyntaxException, IOException {
+        String guestPricesString = Files.readString(Path.of(RoomOccupancyManagerTests.class.getResource("/guestsPrices.csv").toURI()));
+
+        if (guestPricesString != null && !guestPricesString.isEmpty()) {
+            String[] guestPricesArray = guestPricesString.split(",");
+
+            guestsPrices = Arrays.stream(guestPricesArray).map(BigDecimal::new).collect(Collectors.toList());
+        } else {
+            guestsPrices = Collections.emptyList();
+        }
+    }
 
     @Test
     public void shouldReturnZeroRoomsOccupancyAndProfitWhenThereAreNoRoomsAndNoGuestsPrices() {
         // given
-        BigDecimal roomCategoryDelimiter = BigDecimal.valueOf(100);
-        RoomOccupancyManager roomOccupancyManager = new RoomOccupancyManager(new DefaultRoomOccupancyStrategy(roomCategoryDelimiter));
+        RoomOccupancyManager roomOccupancyManager = new RoomOccupancyManager(new DefaultRoomOccupancyStrategy(ROOM_CATEGORY_DELIMITER));
 
         // when
         RoomOccupancyWithProfit roomOccupancyWithProfit = roomOccupancyManager.calculateRoomOccupancyAndProfit(0, 0, emptyList());
@@ -51,13 +61,12 @@ public class RoomOccupancyManagerTests {
             "3, 8, 3, 7, 1153.99, 90"})
     public void shouldReturnAllRoomsOccupancyAndProfitWhenThereAreRoomsAndGuestsPrices(int numberOfAvailableEconomicRooms, int numberOfAvailablePremiumRooms,
                                                                                        int expectedNumberOfOccupiedEconomicRooms, int expectedNumberOfOccupiedPremiumRooms,
-                                                                                       BigDecimal expectedPremiumRoomsProfit, BigDecimal expectedEconomicRoomsProfit){
+                                                                                       BigDecimal expectedPremiumRoomsProfit, BigDecimal expectedEconomicRoomsProfit) {
         // given
-        BigDecimal roomCategoryDelimiter = BigDecimal.valueOf(100);
-        RoomOccupancyManager roomOccupancyManager = new RoomOccupancyManager(new DefaultRoomOccupancyStrategy(roomCategoryDelimiter));
+        RoomOccupancyManager roomOccupancyManager = new RoomOccupancyManager(new DefaultRoomOccupancyStrategy(ROOM_CATEGORY_DELIMITER));
 
         // when
-        RoomOccupancyWithProfit roomOccupancyWithProfit = roomOccupancyManager.calculateRoomOccupancyAndProfit(numberOfAvailableEconomicRooms, numberOfAvailablePremiumRooms, GUESTS_PRICES);
+        RoomOccupancyWithProfit roomOccupancyWithProfit = roomOccupancyManager.calculateRoomOccupancyAndProfit(numberOfAvailableEconomicRooms, numberOfAvailablePremiumRooms, guestsPrices);
 
         // then
         assertEquals(expectedNumberOfOccupiedEconomicRooms, roomOccupancyWithProfit.economicRoomsOccupancy());
