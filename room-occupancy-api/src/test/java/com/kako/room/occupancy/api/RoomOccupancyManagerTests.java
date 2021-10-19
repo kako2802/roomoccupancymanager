7735f1421b9c09ed06c182539772b4c723285d7d
Karol Kowalik <karol.kowalik.280285@gmail.com>
2021-10-19 18:36:36 +0200

    002-create-room-occupancy-api adding room occupancy manager implementation and tests, changing name of module

--- room-occupancy-api/src/test/java/com/kako/room/occupancy/api/RoomOccupancyManagerTests.java
+++ room-occupancy-api/src/test/java/com/kako/room/occupancy/api/RoomOccupancyManagerTests.java
@@ -1,13 +1,30 @@
 package com.kako.room.occupancy.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoomOccupancyManagerTests {
+
+    private static final List<BigDecimal> CLIENTS_PRICES =
+            Arrays.asList(
+                    BigDecimal.valueOf(23),
+                    BigDecimal.valueOf(45),
+                    BigDecimal.valueOf(155),
+                    BigDecimal.valueOf(374),
+                    BigDecimal.valueOf(22),
+                    BigDecimal.valueOf(99.99),
+                    BigDecimal.valueOf(100),
+                    BigDecimal.valueOf(101),
+                    BigDecimal.valueOf(115),
+                    BigDecimal.valueOf(209));
 
     @Test
     public void shouldReturnZeroRoomsOccupancyAndProfitWhenThereAreNoRoomsAndNoGuestsPrices() {
@@ -23,5 +40,29 @@ public class RoomOccupancyManagerTests {
         assertEquals(0, roomOccupancyWithProfit.getPremiumRoomsOccupancy());
         assertEquals(BigDecimal.ZERO, roomOccupancyWithProfit.getPremiumRoomProfit());
         assertEquals(BigDecimal.ZERO, roomOccupancyWithProfit.getEconomicRoomProfit());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 3, 3, 3, 738, 167.99",
+            "5, 7, 4, 6, 1054, 189.99",
+            "7, 2, 4, 2, 583, 189.99",
+            "1, 7, 1, 7, 1153.99, 45",
+            "3, 8, 3, 7, 1153.99, 90"})
+    public void shouldReturnAllRoomsOccupancyAndProfitWhenThereAreRoomsAndGuestsPrices(int numberOfAvailableEconomicRooms, int numberOfAvailablePremiumRooms,
+                                                                                       int expectedNumberOfOccupiedEconomicRooms, int expectedNumberOfOccupiedPremiumRooms,
+                                                                                       BigDecimal expectedEconomicRoomsProfit, BigDecimal expectedPremiumRoomsProfit){
+        // given
+        BigDecimal roomCategoryDelimiter = BigDecimal.valueOf(100);
+        RoomOccupancyManager roomOccupancyManager = new RoomOccupancyManager(roomCategoryDelimiter);
+
+        // when
+        RoomOccupancyWithProfit roomOccupancyWithProfit = roomOccupancyManager.calculateRoomOccupancyAndProfit(numberOfAvailableEconomicRooms, numberOfAvailablePremiumRooms, CLIENTS_PRICES);
+
+        // then
+        assertEquals(expectedNumberOfOccupiedEconomicRooms, roomOccupancyWithProfit.getEconomicRoomsOccupancy());
+        assertEquals(expectedNumberOfOccupiedPremiumRooms, roomOccupancyWithProfit.getPremiumRoomsOccupancy());
+        assertEquals(expectedEconomicRoomsProfit, roomOccupancyWithProfit.getPremiumRoomProfit());
+        assertEquals(expectedPremiumRoomsProfit, roomOccupancyWithProfit.getEconomicRoomProfit());
     }
 }
